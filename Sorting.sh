3e869a980865faea354eed3fc576a0ddc9c5c030
Venkatsompalli <sompallivenkataswamy1@gmail.com>
2021-06-27 11:01:49 +0530

    Array in desending order is done

--- Sorting.sh
+++ Sorting.sh
@@ -16,5 +16,29 @@ Dictionary[B]=$B
 Dictionary[C]=$C
 Dictionary[D]=$D
 
-Array=${Dictionary[@]}
-echo $Array
+
+# Static input of Array
+arr=${Dictionary[@]}
+  
+echo "Array in original order"
+echo ${arr[*]}
+  
+# Performing Bubble sort 
+for ((i = 0; i<4; i++))
+do
+      
+    for((j = 0; j<4-i-1; j++))
+    do
+      
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+        then
+            # swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}  
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+  
+echo "Array in Descending order :"
+echo ${arr[*]}
